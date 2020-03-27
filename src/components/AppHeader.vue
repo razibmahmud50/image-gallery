c66c0da4dce3aa3d5956c6e7207361bddd5a91dc
razibmahmud50 <razibmahmud50@gmail.com>
2020-03-27 23:43:18 +0600

    update

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,20 @@
+<template>
+<nav class="app-header ui secondary pointing menu">
+    <router-link class="item" to="/">Home</router-link> |
+    <router-link class="item" to="/about">About</router-link>
+    <div class="right menu">
+      <a class="ui item">
+        Logout
+      </a>
+    </div>
+</nav>
+</template>
+
+<script>
+export default {
+    name: 'AppHeader'
+}
+</script>
+
+<style>
+</style>
